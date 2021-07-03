2f900520a5fa09aa91359b05f08eb34dd7b21c24
tonga <a.gaston.gimenez@gmail.com>
2021-07-03 17:13:09 -0300

    Get uphold token

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { getToken } from './auth/uphold';
 
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-});
+async function init() {
+  dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+  });
+
+  if (
+    process.env.UPHOLD_API_URL === undefined ||
+    process.env.UPHOLD_API_CLIENT_ID === undefined ||
+    process.env.UPHOLD_API_SECRET === undefined
+  ) {
+    throw new Error('Please set uphold credentials in .env file');
+  }
+
+  await getToken(
+    process.env.UPHOLD_API_URL,
+    process.env.UPHOLD_API_CLIENT_ID,
+    process.env.UPHOLD_API_SECRET
+  );
+}
+
+init();
